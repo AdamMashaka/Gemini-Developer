53f535429e8412ac7dc3c6861569686d4f0d4ed9
AdamMashaka <123875933+AdamMashaka@users.noreply.github.com>
2024-08-13 11:18:19 +0300

    new urls.py

--- tichika/urls.py
+++ tichika/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 from .views import notse
+from .views import ask_question
+from .views import chatbot_api
 
 urlpatterns = [
+    path('ask-question/', ask_question, name='ask-question'),
     path('', views.index, name='index'),
     path('notse/', views.notse, name='notse'),
     path('pricing/', views.pricing, name='pricing'),
@@ -17,11 +20,15 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('notse/index.html', views.notse_index, name='notse_index'),
     path('pricing/index.html', views.notse_index, name='pricing_index'),
-    path('notse/pricing.html', views.notse_index, name='notse_pricing'),
-    path('notse/blog.html', views.notse_index, name='notse_blog'),
-    path('pricing/notse.html', views.notse_index, name='pricing_notse'),
+    path('notse/pricing.html', views.notse_pricing, name='notse_pricing'),
+    path('notse/blog.html', views.notse_blog, name='notse_blog'),
+    path('pricing/notse.html', views.pricing_notse, name='pricing_notse'),
     path('pricing/contact.html', views.notse_index, name='pricing_contact'),
+    path('pricing/pricing.html', views.pricing, name='pricing_pricing'),
+    path('chatbot-api/', chatbot_api, name='chatbot_api'),
+    path('api/chatbot-api/', views.chatbot_api, name='chatbot_api'),
     
 
 
 ]
+
